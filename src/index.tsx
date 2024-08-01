d4a17dacdd2ca1e5117331132727fa1dc8dff27e
ocfox <i@ocfox.me>
2024-08-01 16:35:57 +0800

    chore: add comment create db

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { Bindings } from "./bindings";
-import { post, get, del, put } from "./option";
+import { post, get, del } from "./option";
 import { createTable } from "./db";
 import { Page } from "./page";
 
@@ -10,6 +10,7 @@ app.get("/", async (ctx) => {
   return ctx.html(Page(ctx.env.SERVER));
 });
 
+// Visit https://your.site/~create to create the table at first time
 app.get("/~create", async (ctx) => {
   await createTable(ctx.env.DB);
   return ctx.newResponse("Table created");
